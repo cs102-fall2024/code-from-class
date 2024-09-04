61ae383fa0f53fed21ae57ef2f936b3ae307572d
Sarah <sarah.morrisonsmith@gmail.com>
2024-09-04 11:00:36 -0400

    Class 1b Warmup and Class Code
    Uncomment class_1b.cpp to play around with when you do and do not need {}.

---
+++ class_1b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+//int main() {
+  //int top = 3;
+  //int bottom = 2;
+  //std::cout << "My magic number is: " << (float)top/(float)bottom << std::endl;
+
+  //for (int i = 1; i <= 50; i ++)//{
+    //std::cout << "hello" << std::endl;
+    //if (i % 2 == 0){ 
+    //    std::cout << "yay" << std::endl;
+    
+    //std::cout << "boo" << std::endl;
+    //}
+  //}
+
+//  return 0;
+//}
+
+//curly braces are required for functions
+//int main() 
+//    return 0;
---
+++ class_1b_warmup.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  int top = 3;
+  int bottom = 2;
+  std::cout << "My magic number is: " << (float)top/(float)bottom << std::endl;
+
+  return 0;
+}
