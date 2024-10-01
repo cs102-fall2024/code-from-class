0dbd4145dfd49a3a37325613711c0bf50f80a8ab
Sarah <sarah.morrisonsmith@gmail.com>
2024-10-01 14:40:23 -0400

    Warm up from Monday

---
+++ class_5b_warmup.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file class_5b_warmup.cpp
+ * @author Sarah Morrison-Smith
+ * @brief Class 5b Warmup!
+ * @version 0.1
+ * @date 2024-10-01
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Finish the program so that the program reads `size'
+// ints from the user and puts them in an array. Your program
+// should be free from memory errors.
+int main() {
+    int size = 0;
+    cout << "Gimme a size! ";
+    cin >> size;
+
+    for (int index = 0; index < size; index++) {
+
+    }
+
+    return 0;
+}
